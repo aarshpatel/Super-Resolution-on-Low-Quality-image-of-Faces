1ac4570228495b0e44e49cae442a59d6e6a892bf
Aarsh <aarsh@eduroamguest-2-175.sys.cs.local>
2018-04-05 13:21:52 -0400

    Update the loss file

--- loss.py
+++ loss.py
@@ -1,6 +1,11 @@
 """ Implementation of multiple loss fuctions """
 import torch.nn as nn
+import torch
 
 def pixel_loss(input, target, norm_constant):
 	""" Pixel loss => normalized euclidean distance between the output image and the target """
 	return torch.sum((input - target) ** 2) / norm_constant
+
+def perceptual_loss(input, target):
+	""" Perceptual Loss, as proposed by Justin Johnson paper """
+	pass
