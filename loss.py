a37cd97cdf8729e02366c49dfd742c1988de7abe
Aarsh <aarsh@eduroamguest-2-175.sys.cs.local>
2018-04-05 13:20:25 -0400

    updated the loss file

--- loss.py
+++ loss.py
@@ -3,5 +3,4 @@ import torch.nn as nn
 
 def pixel_loss(input, target, norm_constant):
 	""" Pixel loss => normalized euclidean distance between the output image and the target """
-	return nn.MSELoss() / norm_constant
-	
+	return torch.sum((input - target) ** 2) / norm_constant
