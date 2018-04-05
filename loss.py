cf219311d78d9854ca5e18d31b3a11c098f720ad
Aarsh <aarsh@eduroamguest-2-175.sys.cs.local>
2018-04-05 13:29:10 -0400

    Loss method updated

--- loss.py
+++ loss.py
@@ -4,7 +4,7 @@ import torch
 
 def pixel_loss(input, target, norm_constant):
 	""" Pixel loss => normalized euclidean distance between the output image and the target """
-	return torch.sum((input - target) ** 2) / norm_constant
+	return torch.div(torch.sum((input - target) ** 2), norm_constant)
 
 def perceptual_loss(input, target):
 	""" Perceptual Loss, as proposed by Justin Johnson paper """
