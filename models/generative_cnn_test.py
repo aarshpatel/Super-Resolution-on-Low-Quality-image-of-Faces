0efdd5cb941db45aa99d889e473775ecc4787a13
mtsadler <msadler@umass.edu>
2018-04-23 21:44:30 -0400

    updated the GEN CNN to the format described in SRGAN, with B=5

--- models/generative_cnn_test.py
+++ models/generative_cnn_test.py
@@ -10,38 +10,58 @@ class ThreeLayerCNNGen(nn.Module):
         N = 3
         D_in = 5
 
-        self.L1 = nn.Conv2d(N, D_in, kernel_size=(3,3), stride=(1,1), padding=(2,2))
-        self.L2 = nn.PReLU()
-        self.L3 = nn.Conv2d(D_in, D_in, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2))
-        self.L4 = nn.BatchNorm2d(D_in)
-
-    def forward(self,x):
-        out = self.L2(self.L1(x))
-        for i in range(4):
-            out = ResidualBlock.forward(out)
-        out = self.L4(self.L3(out))
+        #Before First residual Block
+        self.block0 = nn.Sequential(
+            nn.Conv2d(N, 64, kernel_size=4, stride=2, padding=1),
+            nn.PReLU()
+        )
+        self.block1 = ResidualBlock(64)
+        self.block2 = ResidualBlock(64)
+        self.block3 = ResidualBlock(64)
+        self.block4 = ResidualBlock(64)
+        self.block5 = ResidualBlock(64)
+        self.block6 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=2),
+            nn.BatchNorm2d(64)
+        )
+        self.block7 = nn.Sequential(
+            nn.Conv2d(64, 12, kernel_size=3, stride=1, padding=2),
+            nn.PixelShuffle(2),
+            nn.PReLU()
+        )
+        self.block8 = nn.Sequential(
+            nn.Conv2d(N, N, kernel_size=3, stride=1, padding=2),
+            nn.PixelShuffle(1),
+            nn.PReLU()
+        )
+        self.out = nn.Conv2d(N, N, kernel_size=4, stride=2, padding=1),
+    def forward(self, x):
+        block0 = self.block0(x)
+        block1 = self.block1(block0)
+        block2 = self.block2(block1)
+        block3 = self.block3(block2)
+        block4 = self.block4(block3)
+        block5 = self.block5(block4)
+        block6 = block5 + self.block6(block5)
+        block7 = self.block7(block6)
+        block8 = self.block8(block7)
+        out = self.out(block8)
         return out
 
 
 class ResidualBlock(nn.Module):
-    expansion = 1
 
-    def __init__(self):
+    def __init__(self, N):
         super(ResidualBlock, self).__init__()
-        N = 3
-        D_in = 5
-        D_out = 5
-        self.layer1 = nn.Conv2d(N, D_in, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2))
-        self.layer2 = nn.BatchNorm2d(D_in)
+        self.layer1 = nn.Conv2d(N, N, kernel_size=3, stride=1, padding=2)
+        self.layer2 = nn.BatchNorm2d(N)
         self.layer3 = nn.PReLU()
-        self.layer4 = nn.Conv2d(N, D_in, kernel_size=(3, 3), stride=(1, 1), padding=(2, 2))
-        self.layer5 = nn.BatchNorm2d(D_in)
+        self.layer4 = nn.Conv2d(N, N, kernel_size=3, stride=1, padding=2)
+        self.layer5 = nn.BatchNorm2d(N)
 
     def forward(self, x):
         residual = x
-
         out = self.layer5(self.layer4(self.layer3(self.layer2(self.layer1(x)))))
         out += residual
         return out
-
 
