18cc31ae99caee73ce4d57882c45b5aec895c2bb
Aarsh <aarsh@1x-nat-vl930-172-30-101-198.wireless.umass.edu>
2018-04-24 22:16:25 -0400

    Updated the models

--- models/three_layer_cnn_baseline.py
+++ models/three_layer_cnn_baseline.py
@@ -1,28 +1,29 @@
 import torch.nn as nn
+from conv_block import ConvBlock
 
 class ThreeLayerCNNBaseline(nn.Module):
     """ 
     Simple three layer baseline that uses Conv Tranpose to upscale the image to the input image size 
     """
 
-    def __init__(self, batchnorm=False):
+    def __init__(self, num_convblocks=1):
         super(ThreeLayerCNNBaseline,self).__init__()
-        N = 3
-        D_in = 5 
-        D_mid = 5
-        D_out = 3
 
-        self.model = nn.Sequential(
-            nn.Conv2d(N, D_in, kernel_size=(5,5), stride=(1,1), padding=(2,2)),
-            nn.BatchNorm2d(D_in),
-            nn.ReLU(),
-            nn.MaxPool2d(2),
-            nn.Conv2d(D_in, D_mid, kernel_size=(5,5), stride=(1,1), padding=(2,2)),
-            nn.BatchNorm2d(D_mid),
-            nn.ReLU(),
-            nn.ConvTranspose2d(D_mid, D_out,kernel_size=(2,2), stride=(2,2))
+        self.layer_1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=1), 
+            nn.MaxPool2d(2)
         )
 
+        inner_cnn_blocks = [ConvBlock(64, 64) for _ in range(num_convblocks)]
+        self.blocks = nn.Sequential(*inner_cnn_blocks)
+
+        self.upsample = nn.ConvTranspose2d(64, 3,kernel_size=6, stride=2, padding=1)
+
     def forward(self,x):
-        out = self.model(x)
+        # input => 3 * 110 * 110
+        out = self.layer_1(x) # 64 * 54 * 54
+        out = self.blocks(out) # 64 * 54 * 54
+        out = self.upsample(out) # 3 * 110 * 110
         return out
+
+
