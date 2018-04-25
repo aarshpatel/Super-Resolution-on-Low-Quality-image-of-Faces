53c792c17a20008a415a70236dde2e0e8a3cbaa1
Aarsh <aarsh@1x-nat-vl930-172-30-101-198.wireless.umass.edu>
2018-04-25 00:38:01 -0400

    update main

--- main.py
+++ main.py
@@ -57,7 +57,8 @@ def train(train_loader, model, loss_type, optimizer, epoch, model_name, vgg_loss
 
 		# measure psnr and loss
 		mse = loss_fn(output, target)
-		psnr = calc_psnr(mse.data[0])
+		psnr = 10 * log10(1 / mse.data[0])
+		# psnr = calc_psnr(mse.data[0])
 		print "PSNR: ", psnr		
 		psnr_meter.update(psnr, input.size(0))
 		losses_meter.update(loss.data[0], input.size(0))
@@ -132,7 +133,8 @@ def validate(val_loader, model, loss_type, epoch, model_name, vgg_loss=None):
 
 		# compute the psnr and loss on the validation set
 		mse = loss_fn(output , target)
-		psnr = calc_psnr(mse.data[0])
+		psnr = 10 * log10(1 / mse.data[0])
+		# psnr = calc_psnr(mse.data[0])
 		psnr_meter.update(psnr, input.size(0))
 		losses_meter.update(loss.data[0], input.size(0))
 
