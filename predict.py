99f010f5b08fa553e4499fdaa6e4b461f450d60a
mtsadler <msadler@umass.edu>
2018-04-30 03:53:22 -0400

    working on predict.py

--- predict.py
+++ predict.py
@@ -12,6 +12,8 @@ from torch.autograd import Variable
 def save_image(input, output, target, filename):
     """ Save the input, output, target image during training """
     all_images = torch.cat((input, output, target))
+    f = open(filename,"w+")
+    f.close()
     vutils.save_image(all_images, filename=filename, normalize=True)
 
 def apply_gaussian_blur(img, radius):
