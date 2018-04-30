81f9a785dd30c2e5f50d05cd3f34b1c88abd5f5a
Aarsh <aarsh@1x-nat-vl930-172-30-93-67.wireless.umass.edu>
2018-04-30 04:42:51 -0400

    Fixed merge

--- predict.py
+++ predict.py
@@ -56,10 +56,14 @@ if __name__ == "__main__":
         output = model(blurred.cuda())
 
         print "Blurred Output Model: ", output.size() 
+        
+        clean = np.array(clean)
+        clean = Variable(transform_normalize(clean)) 
+        clean = clean.unsqueeze(0) 
 
         vutils.save_image(output.data.float(), filename="saved_models/" + str(model_name) + "prediction.jpg", normalize=True)
-        vutils.save_image(blurred.data.float(), filename="saved_models/" + str(model_name) + "ground_truth.jpg", normalize=True)
-        vutils.save_image(torch.from_numpy(np.array(clean)), filename="saved_models/" + str(model_name) + "blurred.jpg", normalize=True)
+        vutils.save_image(blurred.data.float(), filename="saved_models/" + str(model_name) + "blurred.jpg", normalize=True)
+        vutils.save_image(clean.data.float(), filename="saved_models/" + str(model_name) + "ground_truth.jpg", normalize=True)
  
         # save_image(input=input, output=output, target=target, filename=str(model_name) + "Prediction.jpg")
     else:
