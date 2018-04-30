e95e7e9aea2e1e5c51039f3494e1112eca4d510b
mtsadler <msadler@umass.edu>
2018-04-30 01:30:05 -0400

    added gaussian to predict.py

--- predict.py
+++ predict.py
@@ -11,14 +11,17 @@ from scripts.metrics import calc_psnr, calc_ssim
 from scripts.average_meter import AverageMeter
 import os
 import torchvision.utils as vutils
-
-
+from PIL import Image, ImageFilter
 
 def save_image(input, output, target, filename):
     """ Save the input, output, target image during training """
     all_images = torch.cat((input, output, target))
     vutils.save_image(all_images, filename=filename, normalize=True)
 
+def apply_gaussian_blur(img, radius):
+    """ Apply gaussian blur to an image """
+    blur_image = img.filter(ImageFilter.GaussianBlur(radius=radius))
+    return blur_image
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -31,13 +34,16 @@ if __name__ == "__main__":
     model_name = opt.model
     use_cuda = opt.cuda
     test_image = opt.image
-
+    clean = Image.open(test_image)
+    blurred = apply_gaussian_blur(clean, radius=4)
     if os.path.isfile("/saved_models/" + str(model_name) + "/best_model.pth.tar"):
         print("=> loading checkpoint '{}'".format(model_name))
         test_model = torch.load("/saved_models/" + str(model_name) + "/best_model.pth.tar")
         test_model.cuda()
         test_model.eval()
-        output = test_model(test_image)
-        save_image(input=test_image, output=output, target=model_name, filename="sample_prediction_" + str(model_name) + "__" + str(test_image))
+        output = test_model(blurred)
+        save_image(input=blurred, output=output, target=model_name, filename=str(model_name) + "_Prediction__" + str(output))
+        save_image(input=blurred, output=output, target=model_name, filename=str(model_name) + "_Ground_truth__" + str(test_image))
+        save_image(input=blurred, output=output, target=model_name, filename=str(model_name) + "_Blurred__" + str(blurred))
     else:
         print("=> no checkpoint found at '{}'".format("/saved_models/" + str(model_name) + "/best_model.pth.tar"))
