82ef3f3873ac5b3474fad1f78bb076943a3484c9
mtsadler <msadler@umass.edu>
2018-04-30 02:23:06 -0400

    fixed a small bug 2 in predict.py

--- predict.py
+++ predict.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
     if os.path.isfile("saved_models/" + str(model_name) + "model_best.pth.tar"):
         print("=> loading checkpoint '{}'".format(model_name))
         checkpoint = torch.load("saved_models/" + str(model_name) + "model_best.pth.tar")
-        model = resnet_subpixel_cnn.ResnetSubPixelCNN()
+        model = resnet_subpixel_cnn.ResnetSubPixelCNN(num_resnet_blocks=5)
         model.load_state_dict(checkpoint['state_dict'])
         model.cuda()
         output = model(blurred)
